0c466cff9685fc3718bcf18213ebe803b6c7a20b
Ryan Hebel <20445594+rkhebel@users.noreply.github.com>
2024-11-23 10:20:21 -0700

    fix: Add animation of a point moving from (0,0) to (1,1) and back.

--- three_body_problem.py
+++ three_body_problem.py
@@ -20,4 +20,26 @@
      fig, animate, frames=4, interval=500, init_func=init
  )
 
- plt.show()
+ plt.show()import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+fig, ax = plt.subplots()
+ax.set_xlim(-1, 2)
+ax.set_ylim(-1, 2)
+point, = ax.plot([], [], 'o')
+
+def init():
+    point.set_data([], [])
+    return point,
+
+def animate(i):
+    x = i % 2
+    y = i % 2
+    point.set_data(x, y)
+    return point,
+
+ani = animation.FuncAnimation(
+    fig, animate, frames=4, interval=500, init_func=init
+)
+
+plt.show()
